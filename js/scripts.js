63ceb0a0dd75e34b68c737042abfa1a79642413a
Adrianna Vu <advu@live.unc.edu>
2018-10-11 23:28:55 -0400

    added plant quiz to europe slide

--- js/scripts.js
+++ js/scripts.js
@@ -40,6 +40,100 @@ $(document).ready(function() {
   });
 });
 
+// EUROPE SLIDE
+// Quiz to see if user can differentiate between potato plant and belladonna plant
+
+var myQuestions = [{
+  question: "Can you identify the potato plant?",
+  answers: {
+    A: 'This plant?',
+    B: 'Or maybe this plant?',
+  },
+  correctAnswer: 'B'
+}];
+
+var quizContainer = document.getElementById('plantQuiz');
+var resultsContainer = document.getElementById('plantQuizResults');
+var submitButton = document.getElementById('plantQuizSubmit');
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
+
+  function showQuestions(questions, quizContainer) {
+    // we'll need a place to store the output and the answer choices
+    var output = [];
+    var answers;
+
+    // for each question...
+    for (var i = 0; i < questions.length; i++) {
+
+      // first reset the list of answers
+      answers = [];
+
+      // for each available answer...
+      for (letter in questions[i].answers) {
+
+        // ...add an html radio button
+        answers.push(
+          '<label>' +
+          '<input type="radio" name="question' + i + '" value="' + letter + '">' +
+          letter + ': ' +
+          questions[i].answers[letter] +
+          '</label>'
+        );
+      }
+
+      // add this question and its answers to the output
+      output.push(
+        '<div class="question">' + questions[i].question + '</div>' +
+        '<div class="answers">' + answers.join('') + '</div>'
+      );
+    }
+
+    // finally combine our output list into one string of html and put it on the page
+    quizContainer.innerHTML = output.join('');
+  }
+
+
+  function showResults(questions, quizContainer, resultsContainer) {
+
+    // gather answer containers from our quiz
+    var answerContainers = quizContainer.querySelectorAll('.answers');
+
+    // keep track of user's answers
+    var userAnswer = '';
+    var numCorrect = 0;
+
+    // for each question...
+    for (var i = 0; i < questions.length; i++) {
+
+      // find selected answer
+      userAnswer = (answerContainers[i].querySelector('input[name=question' + i + ']:checked') || {}).value;
+
+      // if answer is correct
+      if (userAnswer === questions[i].correctAnswer) {
+        // add to the number of correct answers
+        numCorrect++;
+      }
+
+    }
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length + '. (A) is belladonna. (B) is potato.';
+  }
+
+  // show questions right away
+  showQuestions(questions, quizContainer);
+
+  // on submit, show results
+  submitButton.onclick = function() {
+    showResults(questions, quizContainer, resultsContainer);
+  }
+
+}
+
+// IRISH SLIDE
 // When user clicks preFamineIreland, display PostFamineIreland instead with legend
 preFamineIrelandImg.addEventListener('click', function() {
   console.log('preFamineIrelandClicked');
